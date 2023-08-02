2ed242e249daa8b97ba3348937ce185d726ed81a
Dubjay18 <jejeniyi7@gmail.com>
2023-08-02 16:57:49 +0100

    chore:removed dash

--- src/app/dash/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Sidebar from "../components/main/Sidebar";
-
-function Main() {
-  return <h1 className='bg-black'>Welcome to sanctum</h1>;
-}
-
-export default Main;
