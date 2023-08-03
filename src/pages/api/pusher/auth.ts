7493ad92280c1e5064ce8399e287c661088068f6
Dubjay18 <jejeniyi7@gmail.com>
2023-08-03 19:31:00 +0100

    rollback

--- src/pages/api/pusher/auth.ts
+++ src/pages/api/pusher/auth.ts
@@ -1,44 +1,33 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { getServerSession } from "next-auth";
+
 import { pusherServer } from "@/lib/pusher";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
+  request: NextApiRequest,
+  response: NextApiResponse
 ) {
-  try {
-    const session = await getServerSession(
-      req,
-      res,
-      authOptions
-    );
+  const session = await getServerSession(
+    request,
+    response,
+    authOptions
+  );
 
-    if (!session?.user?.email) {
-      return res
-        .status(401)
-        .json({
-          error: "Unauthorized: No session or user email.",
-        });
-    }
-
-    const { socket_id: socketId, channel_name: channel } =
-      req.body;
-    const data = {
-      user_id: session.user.email,
-    };
-
-    const authResponse = pusherServer.authorizeChannel(
-      socketId,
-      channel,
-      data
-    );
-
-    return res.json(authResponse);
-  } catch (error) {
-    console.error("Error in Pusher authentication:", error);
-    return res
-      .status(500)
-      .json({ error: "Internal Server Error" });
+  if (!session?.user?.email) {
+    return response.status(401);
   }
+
+  const socketId = request.body.socket_id;
+  const channel = request.body.channel_name;
+  const data = {
+    user_id: session.user.email,
+  };
+
+  const authResponse = pusherServer.authorizeChannel(
+    socketId,
+    channel,
+    data
+  );
+  return response.send(authResponse);
 }
